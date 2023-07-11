6dc0f917e343e1fcc20c7ff82a0bfc2007c999e3
Wallison Viana <wallviana01@gmail.com>
2023-07-11 18:36:11 -0300

    Adicionando funcionalidade de seguidores

--- src/js/objects/screen.js
+++ src/js/objects/screen.js
@@ -6,6 +6,10 @@ const screen = {
                                             <div class="data">
                                                 <h1>${user.name ?? 'Não possui nome cadastrado'}</h1>
                                                 <p>${user.bio ?? 'Não possui bio cadastrada'}</p>
+                                                <div class="follow">
+                                                    <p>Seguidores: ${user.followers}</p>
+                                                    <p>Seguindo: ${user.following}</p>
+                                                </div>
                                             </div>
                                         </div>`
 
